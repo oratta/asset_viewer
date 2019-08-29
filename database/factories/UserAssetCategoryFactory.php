48a00832634a681124842fd3291e4f1a0afb1f27
oratta <oratta@gmail.com>
2019-08-29 17:20:53 +0900

    [WIP]UserAssetCategoryモデル作成
    php artisan make:model UserAssetCategory --migration --factory

---
+++ database/factories/UserAssetCategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\UserAssetCategory;
+use Faker\Generator as Faker;
+
+$factory->define(UserAssetCategory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
