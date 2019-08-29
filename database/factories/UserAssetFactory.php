9435f7e44a4851f6a91a81b5d58c54bec7956f81
oratta <oratta@gmail.com>
2019-08-29 17:19:10 +0900

    [WIP]UserAssetモデル作成
    php artisan make:model UserAsset --migration
    php artisan make:factory UserAssetFactory --model=UserAsset

---
+++ database/factories/UserAssetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\UserAsset;
+use Faker\Generator as Faker;
+
+$factory->define(UserAsset::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2019_08_29_075848_create_user_assets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_assets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('user_asset_category_ids');
+            $table->text('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_assets');
+    }
+}
