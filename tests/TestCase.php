7bce04ab045173f0e28496f30c8e6ee7821de07b
oratta <oratta@gmail.com>
2019-09-05 16:38:51 +0900

    シーダーが実行されない不具合修正

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,32 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    static protected $databaseSetup = false;
     use CreatesApplication;
-
-//    public function setUp() : void
-//    {
-//        if (! $this->app) {
-//            $this->refreshApplication();
-//        }
-//
-//        // 最初のみ実行するフラグを追加
-//        if (!static::$databaseSetup) {
-//            Artisan::call('migrate:refresh --seed');
-//            static::$databaseSetup = true;
-//        }
-//    }
 
     public function setUp() : void
     {
         parent::setUp();
 
         $uses = array_flip(class_uses_recursive(static::class));
-        if(isset($uses[SeedingDatabase::class]) && !static::$databaseSetup){
+        if(isset($uses[SeedingDatabase::class])){
             $this->seedingDatabase();
         }
     }
