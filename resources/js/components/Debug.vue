b56198c847acba4ae36e820eb6b84f54530aab1f
oratta <oratta@gmail.com>
2019-09-06 20:01:44 +0900

    デバッグメニューをフッターに表示

---
+++ resources/js/components/Debug.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="debug">
+        <h2>Debug Menue</h2>
+        <div class="debug__item">
+            <h3>Data set</h3>
+            <form class="form" @submit.prevent="addAsset">
+                <label for="asset_count">asset count</label>
+                <input type="text" class="form__item" id="asset_count" v-model="addAssetForm.assetCount">
+                <div class="form__button">
+                    <button type="submit" class="button button--inverse">submit</button>
+                </div>
+            </form>
+            <p>ログインユーザのUserAssetをランダムで設定します</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { CREATED } from '../util'
+    export default{
+        data(){
+            return {
+                addAssetForm: {
+                    assetCount: 0,
+                }
+            }
+        },
+        methods: {
+            async addAsset () {
+                const response = await axios.post('/api/debug/addCount', this.addAssetForm)
+
+                if (response.status !== CREATED){
+                    this.$store.commit('error/setCode', response.status)
+                    return false
+                }
+                this.$store.commit('debug/setObj', response)
+            },
+        },
+    }
+</script>
--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,18 +1,25 @@
 <template>
     <footer class="footer">
-        <button v-if="isLogin" class="button button--link" @click="logout">
-            Logout
-        </button>
-        <RouterLink v-else class="button button--link" to="/login">
-            Login / Register
-        </RouterLink>
+        <div class="footer">
+            <button v-if="isLogin" class="button button--link" @click="logout">
+                Logout
+            </button>
+            <RouterLink v-else class="button button--link" to="/login">
+                Login / Register
+            </RouterLink>
+        </div>
+        <Debug/>
     </footer>
 </template>
 
 <script>
     import { mapState, mapGetters } from 'vuex'
+    import Debug from '../components/Debug.vue'
 
     export default {
+        components: {
+            Debug
+        },
         computed: {
             ...mapState({
                 apiStatus: state => state.auth.apiStatus
