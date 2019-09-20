99f91a6b66df533f93c6e5327314f02c2ddb9468
oratta <oratta@gmail.com>
2019-09-20 16:21:09 +0900

    [WIP]submitボタンと機能の作成

--- resources/js/pages/UserAsset.vue
+++ resources/js/pages/UserAsset.vue
@@ -2,39 +2,47 @@
     <div class="user-asset">
         <div class="container">
             <h2>Your Asset List</h2>
-            <table>
-                <thead>
-                    <th>account name</th>
-                    <th>asset name</th>
-                    <th>value</th>
-                    <th v-for="(section, sectionId) in this.sections">
-                        {{ section.name }}
-                    </th>
-                </thead>
-                <tbody>
-                    <tr v-for="userAsset in this.userAssets">
-                        <td class="user-asset__data">
-                            {{ userAsset.account }}
-                        </td>
-                        <td class="user-asset__data">
-                            {{ userAsset.name }}
-                        </td>
-                        <td class="user-asset__data">
-                            {{ userAsset.value }}
-                        </td>
-                        <td v-for="categories in userAsset.sectionInfos">
-                            <div class="user-asset__select sl">
-                                <select :name="userAsset.id + '-' + categories[0].section_id">
-                                    <option selected="selected">select...</option>
-                                    <option v-for="category in categories" :value="category.id">
-                                        {{ category.name }}
-                                    </option>
-                                </select>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div>
+                <form class="form" @submit.prevent="categorize">
+                    <table>
+                        <thead>
+                        <th>account name</th>
+                        <th>asset name</th>
+                        <th>value</th>
+                        <th v-for="(section, sectionId) in this.sections">
+                            {{ section.name }}
+                        </th>
+                        </thead>
+                        <tbody>
+                        <tr v-for="userAsset in this.userAssets">
+                            <td class="user-asset__data">
+                                {{ userAsset.account }}
+                            </td>
+                            <td class="user-asset__data">
+                                {{ userAsset.name }}
+                            </td>
+                            <td class="user-asset__data">
+                                {{ userAsset.value }}
+                            </td>
+                            <td v-for="categories in userAsset.sectionInfos">
+                                <div class="user-asset__select sl">
+                                    <select v-model="userAsset[categories[0].section_id]">
+                                        <option selected="selected">select...</option>
+                                        <option v-for="category in categories" :value="category.id">
+                                            {{ category.name }}
+                                        </option>
+                                    </select>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div class="form__button">
+                        <button type="submit" class="button button--inverse">categorize</button>
+                    </div>
+                </form>
+            </div>
+
         </div>
     </div>
 </template>
@@ -44,11 +52,24 @@
     export default {
         data () {
             return {
-                uAssets: [],
                 sections: [],
                 sectionInfos: [],
                 userAssets: [],
             }
+        },
+        computed: {
+            categorizeForm(){
+                console.log("categorizeForm:start to create form data")
+                var returnArr = {}
+                this.userAssets.forEach(function(uAsset){
+                    console.log("categorizeForm:in assets loop")
+                    for (var sectionId=1;sectionId<=3;sectionId++){
+                        var key = uAsset.id + "-" + sectionId
+                        returnArr[key] = uAsset[sectionId]
+                    }
+                })
+                return returnArr
+            },
         },
         methods: {
             async fetchUserAsset () {
@@ -64,6 +85,9 @@
                 for (var i = 0, len = this.userAssets.length; i < len; ++i) {
                     this.userAssets[i].sectionInfos = this.sectionInfos
                 }
+            },
+            async categorize () {
+                const response = await axios.post('/api/categorize', this.categorizeForm)
             },
         },
         watch: {
